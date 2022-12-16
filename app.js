eb395e1bb87fd40f25dd43d0f277cbec50f021b5
i-guerrero <isaacguerrero@pursuit.org>
2022-12-16 13:51:32 -0500

    Complete Catchphrases Express Activity

--- app.js
+++ app.js
@@ -9,5 +9,47 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+// Activity Catchphrases Express
+app.get("/terminator", (req, res) => {
+  res.send("I'll be back. ----- Hasta la vista, baby");
+  //   res.send("Hasta la vista, baby"); - causes error
+});
+
+app.get("/timgunn", (request, response) => {
+  response.send("Make it work");
+});
+
+app.get("/SteveMcGarrett", (request, response) => {
+  response.send("Book 'em Danno!");
+});
+
+app.get("/captainAmerica", (request, response) => {
+  response.send("Avengers Assemble!");
+});
+
+app.get("/spiderman", (request, response) => {
+  response.send("My Spider-Sense is tingling!");
+});
+
+app.get("/regis", (request, response) => {
+  response.send("Is that your final answer?");
+});
+
+app.get("/rocky", (request, response) => {
+  response.send("Yo, Adrian");
+});
+
+app.get("/steverogers", (request, response) => {
+  response.send("I can do this all day");
+});
+
+app.get("/groot", (request, response) => {
+  response.send("I am groot");
+});
+
+app.get("/drstrange", (request, response) => {
+  response.send("Dormammu, I've come to bargain");
+});
+
 // EXPORT
 module.exports = app;
