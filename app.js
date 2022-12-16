127a76a524fbff384669c54e9a0663602542f06d
i-guerrero <isaacguerrero@pursuit.org>
2022-12-16 13:58:25 -0500

    Add Magic8 route and implementation

--- app.js
+++ app.js
@@ -51,5 +51,33 @@ app.get("/drstrange", (request, response) => {
   response.send("Dormammu, I've come to bargain");
 });
 
+// Activity Magic 8 Ball
+
+const magic8Responses = [
+  "It is certain",
+  "It is decidedly so",
+  "Without a doubt",
+  "Yes - Definitely",
+  "You may rely on it",
+  "As I see it, yes",
+  "Most likely",
+  "Outlook good",
+  "Yes, and signs point to yes",
+  "Reply hazy, try again",
+  "Ask again later",
+  "Better not tell you now",
+  "Cannot predict now",
+  "Concentrate and ask again",
+  "Don't count on it",
+  "My reply is no",
+  "My sources say no",
+  "Outlook not so good",
+  "Very doubtful",
+];
+
+app.get("/magic8", (req, res) => {
+  res.send(`<h1>${magic8Responses[Math.floor(Math.random() * 19)]}</h1>`);
+});
+
 // EXPORT
 module.exports = app;
